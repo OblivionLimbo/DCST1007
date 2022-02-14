725c10e3310d392c0dd2d49488a12c84a90568fd
Oblivion <60346441+OblivionLimbo@users.noreply.github.com>
2022-02-14 14:47:47 +0100

    OOP5 Table Done

--- Tasks/OOP5.js
+++ Tasks/OOP5.js
@@ -1,22 +1,40 @@
  fetch("fylkestopper.json")
     .then(response => response.json())
-    .then(data => console.log(data))
-    .then(json => {showTable(json); /*showMap(json)*/})
+    .then(json => showTable(json))
     .catch(error => showError(error));
 
-    function showError(x){
-        console.log(x);
+    function showError(error){
+        console.log(error);
     }
 
-    function showTable(x){
+    // Table
+
+    function showTable(fylkestopper){
         let table = document.createElement("table");
+            table.classList.add("niceTable");
         let thead = table.createTHead()
         let row = thead.insertRow()
-            
-        x.sort(function(a,b) {
+        let cell = row.insertCell()
+            cell.innerHTML = "<b>Fylke</b>"
+            cell = row.insertCell()
+            cell.innerHTML = "<b>Fjelltopp</b>"
+            cell = row.insertCell()
+            cell.innerHTML = "<b>Høyde</b>"
+        fylkestopper.sort(function(a,b) {
             return b.hoyde - a.hoyde;
         })
-        
+        for(let i in fylkestopper){
+            let fylke = fylkestopper[i]
+            row = table.insertRow()
+            console.log(fylke)
+            cell = row.insertCell()
+            cell.innerHTML = fylke.fylke
+            cell = row.insertCell()
+            cell.innerHTML = fylke.fjell
+            cell = row.insertCell()
+            cell.innerHTML = fylke.hoyde + " m"
+        }
+        document.getElementById("mountains").appendChild(table);
     }
 
-    document.getElementById("test").onclick = () => console.log("hei");
+    // Map
