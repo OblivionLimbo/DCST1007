77b5ac94719e94a4bf89a5e55a4e280b0fdafe8d
Oblivion <60346441+OblivionLimbo@users.noreply.github.com>
2022-02-15 14:08:58 +0100

    map kinda

--- Tasks/OOP5.js
+++ Tasks/OOP5.js
@@ -1,6 +1,6 @@
  fetch("fylkestopper.json")
     .then(response => response.json())
-    .then(json => showTable(json))
+    .then(json => {showTable(json),showMap(json)})
     .catch(error => showError(error));
 
     function showError(error){
@@ -26,7 +26,6 @@
         for(let i in fylkestopper){
             let fylke = fylkestopper[i]
             row = table.insertRow()
-            console.log(fylke)
             cell = row.insertCell()
             cell.innerHTML = fylke.fylke
             cell = row.insertCell()
@@ -39,4 +38,37 @@
 
     // Map
 
-    
+    var map = new OpenLayers.Map('map');
+    map.addLayer(new OpenLayers.Layer.OSM())
+
+    epsg4326 =  new OpenLayers.Projection("EPSG:4326"); //transformerer fra WGS84
+    projectTo = map.getProjectionObject(); //transformerer til kartets koordinatsystem (sfærisk mercator)
+
+    let lonLat = new OpenLayers.LonLat(9.6807076, 61.8223407).transform(epsg4326, projectTo); //Sentrum midt i norge
+    map.setCenter(lonLat, 6);    
+
+    let markers = new OpenLayers.Layer.Markers("Markers"); // Lag et nytt lag å lagre markørene i
+    map.addLayer(markers);
+
+    function showMap(fylkestopper) {
+        for(let fylke of fylkestopper) {
+            let lonlat = new OpenLayers.LonLat(fylke.long, fylke.lat).transform(epsg4326, projectTo); //Hent koordinatene fra fylkestopp-objektet og lag markør
+            markers.addMarker(new OpenLayers.Marker(lonlat));
+        }
+    }
+    // var map = new ol.Map({
+    //     target: 'map',
+    //     layers: [
+    //       new ol.layer.Tile({
+    //         source: new ol.source.OSM()
+    //       }),
+    //       new ol.layer.Tile({
+    //           source: new ol.source.OSM()
+    //       })
+    //     ],
+    //     view: new ol.View({
+    //         // "lat": "60.13518", "long": "10.619047"
+    //       center: ol.proj.fromLonLat([10.619047,60.13518]),
+    //       zoom: 5
+    //     })
+    //   });
