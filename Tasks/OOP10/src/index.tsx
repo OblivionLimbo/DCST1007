c9071380e582d78d27d5d4ad3f7b24ffee6365a5
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-04-19 10:10:10 +0200

    Started work on OOP10

--- Tasks/OOP10/src/index.tsx
+++ Tasks/OOP10/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Component } from 'react-simplified';
 import ReactDOM from 'react-dom';
 import { NavLink, HashRouter, Route } from 'react-router-dom';
-import { Student, studentService } from './services';
+import { Student, studentService, Group, groupService } from './services';
 import { Alert, Card, Row, Column, NavBar, Button, Form } from './widgets';
 import { createHashHistory } from 'history';
 
@@ -13,6 +13,7 @@ class Menu extends Component {
     return (
       <NavBar brand="StudAdm">
         <NavBar.Link to="/students">Students</NavBar.Link>
+        <NavBar.Link to="/groups">Groups</NavBar.Link>
       </NavBar>
     );
   }
@@ -29,6 +30,7 @@ class StudentList extends Component {
 
   render() {
     return (
+      <>
       <Card title="Students">
         {this.students.map((student) => (
           <Row key={student.id}>
@@ -38,6 +40,8 @@ class StudentList extends Component {
           </Row>
         ))}
       </Card>
+      <Button.Success onClick={() => history.push('/students/create')}>Create New Student</Button.Success>
+     </>
     );
   }
 
@@ -50,6 +54,7 @@ class StudentList extends Component {
 
 class StudentDetails extends Component<{ match: { params: { id: string } } }> {
   student = new Student();
+  groupName: string = '';
 
   render() {
     return (
@@ -63,6 +68,10 @@ class StudentDetails extends Component<{ match: { params: { id: string } } }> {
             <Column width={2}>Email:</Column>
             <Column>{this.student.email}</Column>
           </Row>
+          <Row>
+            <Column width={2}>Group:</Column>
+            <Column><NavLink to={'/group/' + this.student.groupId}>{this.groupName}</NavLink></Column>
+          </Row>
         </Card>
         <Button.Light onClick={this.edit}>Edit</Button.Light>
       </div>
@@ -72,6 +81,9 @@ class StudentDetails extends Component<{ match: { params: { id: string } } }> {
   mounted() {
     studentService.getStudent(Number(this.props.match.params.id), (student) => {
       this.student = student;
+      studentService.getGroup(student.groupId, (group) => {
+        this.groupName = group;
+      });
     });
   }
 
@@ -107,6 +119,9 @@ class StudentEdit extends Component<{ match: { params: { id: string } } }> {
           <Column right>
             <Button.Light onClick={this.cancel}>Cancel</Button.Light>
           </Column>
+          <Column>
+            <Button.Danger onClick={this.delete}>Delete</Button.Danger>
+          </Column>
         </Row>
       </div>
     );
@@ -124,10 +139,288 @@ class StudentEdit extends Component<{ match: { params: { id: string } } }> {
     });
   }
 
+  delete() {
+    studentService.deleteStudent(this.student.id, () => {
+      history.push('/students');
+    });
+  }
+
   cancel() {
     history.push('/students/' + this.props.match.params.id);
   }
 }
+
+class CreateStudent extends Component {
+  student = new Student();
+  groups: Group[] = [];
+
+  render() {
+    return (
+      <div>
+        <Card title="Create student">
+          <Form.Label>Name:</Form.Label>
+          <Form.Input
+            type="text"
+            value={this.student.name}
+            onChange={(event) => (this.student.name = event.currentTarget.value)}
+          />
+          <Form.Label>Email:</Form.Label>
+          <Form.Input
+            type="text"
+            value={this.student.email}
+            onChange={(event) => (this.student.email = event.currentTarget.value)}
+          />
+          <Form.Label>Group</Form.Label>
+          <br></br>
+            <select value={this.student.groupId} onChange={(event) => (this.student.groupId = event.currentTarget.value)}>
+              {this.groups.map((groups) => (
+                <option key={groups.id} value={groups.id}>
+                  {groups.name}
+                </option>
+              ))}
+            </select>
+        </Card>
+        <Row>
+          <Column>
+            <Button.Success onClick={this.save}>Save</Button.Success>
+          </Column>
+          <Column right>
+            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
+          </Column>
+        </Row>
+      </div>
+    );
+  }
+
+  save() {
+    studentService.createStudent(this.student, () => {
+      history.push('/students');
+    });
+  }
+
+  cancel() {
+    history.push('/students');
+  }
+
+  mounted() {
+    groupService.getGroups((groups) => {
+      this.groups = groups;
+    });
+  } 
+}
+
+class GroupList extends Component <{ match: { params: { id: string } } }> {
+  groups: Group[] = [];
+
+  render() {
+    return (
+      <>
+      <Card title="Groups">
+        {this.groups.map((group) => (
+          <Row key={group.id}>
+            <Column>
+              <NavLink to={'/groups/' + group.id}>{group.name}</NavLink>
+            </Column>
+          </Row>
+        ))}
+      </Card>
+      <Button.Success onClick={() => history.push('/group/create')}>Create New Group</Button.Success>
+     </>
+    );
+  }
+
+  mounted() {
+    groupService.getGroups((groups) => {
+      this.groups = groups;
+    });
+  }
+}
+
+class GroupDetails extends Component<{ match: { params: { id: string } } }> {
+  group = new Group();
+  members: Student[] = [];
+  leader: string = '';
+  
+  render() {
+    return (
+      <>
+      <div>
+        <Card title="Group details">
+          <Row>
+            <Column width={2}>Name:</Column>
+            <Column>{this.group.name}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>Description:</Column>
+            <Column>{this.group.description}</Column>
+          </Row>
+          <Row>
+            <Column width={2}>Leader:</Column>
+            <NavLink to={'/students/' + this.group.leaderId}>
+            <Column>{this.leader}</Column>
+            </NavLink>
+          </Row>
+          <Row>
+            <Column width={2}>Image:</Column>
+            <Column><img src={this.group.groupImage}></img></Column>
+          </Row>
+          <br />
+          <Row>
+            <Column width={2}><b>Members:</b></Column>
+            {this.members.map((member) => (
+              <Row key={member.id}>
+                <Column>
+                  <NavLink to={'/students/' + member.id}>{member.name}</NavLink>
+                </Column>
+              </Row>
+            ))}
+          </Row>
+        </Card>
+        <Button.Light onClick={this.edit}>Edit</Button.Light>
+      </div>
+      </>
+    );
+  }
+
+  mounted() {
+    groupService.getGroup(Number(this.props.match.params.id), (group) => {
+      this.group = group;
+      groupService.getMembers(group.id, (students) => {
+        this.members = students;
+      });
+    });
+    groupService.getLeader(Number(this.props.match.params.id), (leader) => {
+      this.leader = leader;
+    }
+    );
+  }
+
+  edit() {
+    history.push('/groups/' + this.group.id + '/edit');
+  }
+}
+
+class GroupEdit extends Component<{ match: { params: { id: number } } }> {
+  
+  group = new Group();
+  members: Student[] = [];
+
+  render() {
+    if (!this.group) return null;
+
+    return (
+      <div>
+        <Card title="Update Group">
+          <Form.Label>Name:</Form.Label>
+          <Form.Input
+            type="text"
+            value={this.group.name}
+            onChange={(event) => (this.group.name = event.currentTarget.value)}/>
+          <Form.Label>Leader:</Form.Label>
+          <br></br>
+            <select value={this.group.leaderId} onChange={(event) => (this.group.leaderId = event.currentTarget.value)}>
+              {this.members.map((member) => (
+                <option key={member.id} value={member.id}>
+                  {member.name}
+                </option>
+              ))}
+            </select>
+          <br></br>
+          <Form.Label>Description:</Form.Label>
+          <Form.Input 
+          type="text" 
+          value={this.group.description} 
+          onChange={(event) => (this.group.description = event.currentTarget.value)} />
+          <Form.Label>Group Image</Form.Label>
+          <Form.Input 
+          type="text" 
+          value={this.group.groupImage} 
+          onChange={(event) => (this.group.groupImage = event.currentTarget.value)} />
+        </Card>
+        <Row>
+          <Column>
+            <Button.Success onClick={this.save}>Save</Button.Success>
+          </Column>
+          <Column>
+            <Button.Danger onClick={this.delete}>
+              Delete
+            </Button.Danger>
+          </Column>
+          <Column right>
+            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
+          </Column>
+        </Row>
+      </div>
+    );
+  }
+
+  mounted() {
+    groupService.getGroup(this.props.match.params.id, (group) => {
+      this.group = group;
+      groupService.getMembers(this.props.match.params.id, (members) => {
+        this.members = members;
+      });
+    });
+  }
+
+  save() {
+    groupService.updateGroup(this.group, () => {
+      history.push('/group/' + this.props.match.params.id);
+    });
+  }
+  delete() {
+    groupService.deleteGroup(this.group.id, () => {
+      history.push('/group');
+    });
+  }
+
+  cancel() {
+    history.push('/group/' + this.props.match.params.id);
+  }
+}
+
+class CreateGroup extends Component {
+  group = new Group();
+
+  render() {
+    return (
+      <div>
+        <Card title="Create group">
+          <Form.Label>Name:</Form.Label>
+          <Form.Input
+            type="text"
+            value={this.group.name}
+            onChange={(event) => (this.group.name = event.currentTarget.value)}
+          />
+          <Form.Label>Leader</Form.Label>
+
+          <Form.Label>Description</Form.Label>
+
+          <Form.Label>Image</Form.Label>
+        </Card>
+        <Row>
+          <Column>
+            <Button.Success onClick={this.save}>Save</Button.Success>
+          </Column>
+          <Column right>
+            <Button.Light onClick={this.cancel}>Cancel</Button.Light>
+          </Column>
+        </Row>
+      </div>
+    );
+  }
+  
+  save() {
+    groupService.createGroup(this.group, () => {
+      history.push('/groups');
+    });
+  }
+
+  cancel() {
+    history.push('/groups');
+  }
+}
+
 
 ReactDOM.render(
   <div>
@@ -139,6 +432,11 @@ ReactDOM.render(
         <Route exact path="/students" component={StudentList} />
         <Route exact path="/students/:id" component={StudentDetails} />
         <Route exact path="/students/:id/edit" component={StudentEdit} />
+        <Route exact path="/students/create" component={CreateStudent} />
+        <Route exact path="/groups" component={GroupList} />
+        <Route exact path="/group/:id" component={GroupDetails} />
+        <Route exact path="/group/:id/edit" component={GroupEdit} />
+        <Route exact path="/group/create" component={CreateGroup} />
       </div>
     </HashRouter>
   </div>,
--- Tasks/OOP10/src/services.tsx
+++ Tasks/OOP10/src/services.tsx
@@ -4,6 +4,15 @@ export class Student {
   id: number = 0;
   name: string = '';
   email: string = '';
+  groupId: number = 0;
+}
+
+export class Group {
+  id: number = 0;
+  name: string = '';
+  leaderId: number = 0;
+  description: string = '';
+  groupImage: string = '';
 }
 
 class StudentService {
@@ -23,9 +32,17 @@ class StudentService {
     });
   }
 
+  getGroup(id: number, success: (group: string) => void) {
+    pool.query('SELECT name FROM Groups WHERE id=?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results[0].name);
+    });
+  }
+
   updateStudent(student: Student, success: () => void) {
     pool.query(
-      'UPDATE Students SET name=?, email=? WHERE id=?',
+      'UPDATE Students SET name=?, email=?, groupId=? WHERE id=?',
       [student.name, student.email, student.id],
       (error) => {
         if (error) return console.error(error);
@@ -34,5 +51,93 @@ class StudentService {
       }
     );
   }
+
+  deleteStudent(id: number, success: () => void) {
+    pool.query('DELETE FROM Students WHERE id=?', [id], (error) => {
+      if (error) return console.error(error);
+
+      success();
+    });
+  }
+
+  createStudent(student: Student, success: () => void) {
+    pool.query(
+      'INSERT INTO Students (name, email, groupId) VALUES (?, ?, ?)',
+      [student.name, student.email],
+      (error) => {
+        if (error) return console.error(error);
+
+        success();
+      }
+    );
+  }
 }
+
+class GroupService {
+  getGroups(success: (groups: Group[]) => void) {
+    pool.query('SELECT * FROM Groups', (error, results) => {
+      if (error) return console.error(error);
+
+      success(results);
+    });
+  }
+
+  getGroup(id: number, success: (group: Group) => void) {
+    pool.query('SELECT * FROM Groups WHERE id=?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results[0]);
+    });
+  }
+
+  getMembers(id: number, success: (members: Student[]) => void) {
+    pool.query('SELECT * FROM Students WHERE groupId=?', [id], (error, results) => {
+      if (error) return console.error(error);
+
+      success(results);
+    });
+  }
+  
+  getLeader(id: number, success: (leader: string) => void) {
+    pool.query('SELECT name FROM Students WHERE id=?', [id], (error, results) => {
+      if (error) return console.error(error);
+      
+      success(results[0]);
+    });
+  }
+
+  updateGroup(group: Group, success: () => void) {
+    pool.query(
+      'UPDATE Groups SET name=?, leaderId=?, description=?, groupImage=? WHERE id=?',
+      [group.name, group.leaderId, group.description, group.groupImage, group.id],
+      (error) => {
+        if (error) return console.error(error);
+
+        success();
+      }
+    );
+  }
+
+  deleteGroup(id: number, success: () => void) {
+    pool.query('DELETE FROM Groups WHERE id=?', [id], (error) => {
+      if (error) return console.error(error);
+
+      success();
+    });
+  }
+
+  createGroup(group: Group, success: () => void) {
+    pool.query(
+      'INSERT INTO Groups (name, leaderId, description, groupImage) VALUES (?, ?, ?, ?)',
+      [group.name, group.leaderId, group.description, group.groupImage],
+      (error) => {
+        if (error) return console.error(error);
+
+        success();
+      }
+    );
+  }
+}
+
 export let studentService = new StudentService();
+export let groupService = new GroupService();
