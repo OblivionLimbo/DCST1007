8adc8935956f4c31cd14b490515a2a12deef996b
Kristoffer Juelsen <kristoju@stud.ntnu.no>
2022-04-30 12:58:47 +0200

    Minor changes

--- Tasks/OOP11/src/index.tsx
+++ Tasks/OOP11/src/index.tsx
@@ -7,6 +7,8 @@ export class App extends Component {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: 'center' }}>
+        <h3>Translate text into emojis</h3>
+        <input type="text" value="Hello World" placeholder="Type something..."></input><button>Translate</button>
         <Text>{moji.translate('Hello World')}</Text>
       </View>
     );
